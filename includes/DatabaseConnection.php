f7610aa606b25a18321f039a3ac4f33472ccd469
ChrisSHDev <kakeochee@naver.com>
2021-04-25 17:44:41 -0700

    fixed db

--- includes/DatabaseConnection.php
+++ includes/DatabaseConnection.php
@@ -1,8 +1,6 @@
 <?php
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-var_dump($url);
-
 $pdo = new PDO('mysql:host=us-cdbr-east-03.cleardb.com;dbname=heroku_82998037b0d052c;charset=utf8', 'ba0a05146a31b1', '503df4c5');
 
 $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
